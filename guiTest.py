97720d1198bb10bdef4794a45ff50cb4e4ece4d0
nshameed3 <nah2@umbc.edu>
2018-12-17 22:09:36 -0500

    gui

---
+++ guiTest.py
@@ -0,0 +1,18 @@
+from easygui import *
+
+msg = "Enter your personal information"
+title = "Credit Card Application"
+fieldNames = ["Name","Street Address","City","State","ZipCode"]
+fieldValues = []  # we start with blanks for the values
+fieldValues = multenterbox(msg,title, fieldNames)
+
+# make sure that none of the fields was left blank
+while 1:
+    if fieldValues == None: break
+    errmsg = ""
+    for i in range(len(fieldNames)):
+      if fieldValues[i].strip() == "":
+        errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+    if errmsg == "": break # no problems found
+    fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
+print "Reply was:", fieldValues
