0fb24291b6720c9626c42c7ebaf8af21a8852469
nshameed3 <nah2@umbc.edu>
2018-12-17 22:14:07 -0500

    login box

--- guiTest.py
+++ guiTest.py
@@ -16,3 +16,27 @@ while 1:
     if errmsg == "": break # no problems found
     fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
 print "Reply was:", fieldValues
+
+
+
+
+
+
+
+
+msg = "Enter logon information"
+title = "Demo of multpasswordbox"
+fieldNames = ["Server ID", "User ID", "Password"]
+fieldValues = []  # we start with blanks for the values
+fieldValues = multpasswordbox(msg,title, fieldNames)
+
+# make sure that none of the fields was left blank
+while 1:
+  if fieldValues == None: break
+  errmsg = ""
+  for i in range(len(fieldNames)):
+    if fieldValues[i].strip() == "":
+      errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+  if errmsg == "": break # no problems found
+  fieldValues = multpasswordbox(errmsg, title, fieldNames, fieldValues)
+print "Reply was:", fieldValues
