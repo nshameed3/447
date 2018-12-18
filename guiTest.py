034422138c7b9fb9639e546ba22e6cd69d104984
nshameed3 <nah2@umbc.edu>
2018-12-17 23:01:16 -0500

    added forms for the call staff to add to the database

--- guiTest.py
+++ guiTest.py
@@ -1,8 +1,28 @@
 from easygui import *
 
-msg = "Enter your personal information"
-title = "Credit Card Application"
-fieldNames = ["Name","Street Address","City","State","ZipCode"]
+
+msg = "Enter logon information"
+title = "Demo of multpasswordbox"
+fieldNames = ["User ID", "Password"]
+fieldValues = []  # we start with blanks for the values
+fieldValues = multpasswordbox(msg,title, fieldNames)
+
+# make sure that none of the fields was left blank
+while 1:
+  if fieldValues == None: break
+  errmsg = ""
+  for i in range(len(fieldNames)):
+    if fieldValues[i].strip() == "":
+      errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+  if errmsg == "": break # no problems found
+  fieldValues = multpasswordbox(errmsg, title, fieldNames, fieldValues)
+print "Reply was:", fieldValues
+
+
+
+msg = "Enter event information"
+title = "EVENT REPORT"
+fieldNames = ["Name","Street Address","City","State","ZipCode", "Phone Number" "Description", "Priority"]
 fieldValues = []  # we start with blanks for the values
 fieldValues = multenterbox(msg,title, fieldNames)
 
@@ -20,23 +40,38 @@ print "Reply was:", fieldValues
 
 
 
-
-
-
-
-msg = "Enter logon information"
-title = "Demo of multpasswordbox"
-fieldNames = ["User ID", "Password"]
+msg = "Enter equipment information"
+title = "EQUIPMENT REPORT"
+fieldNames = ["Owner Name","Owner Phone Number","Type", "Description", "Condition"]
 fieldValues = []  # we start with blanks for the values
-fieldValues = multpasswordbox(msg,title, fieldNames)
+fieldValues = multenterbox(msg,title, fieldNames)
 
 # make sure that none of the fields was left blank
 while 1:
-  if fieldValues == None: break
-  errmsg = ""
-  for i in range(len(fieldNames)):
-    if fieldValues[i].strip() == "":
-      errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
-  if errmsg == "": break # no problems found
-  fieldValues = multpasswordbox(errmsg, title, fieldNames, fieldValues)
+    if fieldValues == None: break
+    errmsg = ""
+    for i in range(len(fieldNames)):
+      if fieldValues[i].strip() == "":
+        errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+    if errmsg == "": break # no problems found
+    fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
+print "Reply was:", fieldValues
+
+
+
+msg = "Enter volunteer information"
+title = "VOLUNTEER REPORT"
+fieldNames = ["First Name","Last Name","Phone Number","Street Address","City","State","ZipCode", "Availability"]
+fieldValues = []  # we start with blanks for the values
+fieldValues = multenterbox(msg,title, fieldNames)
+
+# make sure that none of the fields was left blank
+while 1:
+    if fieldValues == None: break
+    errmsg = ""
+    for i in range(len(fieldNames)):
+      if fieldValues[i].strip() == "":
+        errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+    if errmsg == "": break # no problems found
+    fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
 print "Reply was:", fieldValues
